49ebf076866c3ad4ba0f61bfae97e40a09ce68fd
marckenson <marckensonpcivil@hotmail.com>
2015-06-15 19:15:43 -0700

    Revert "Background Screen"
    This reverts commit 721b98fad4e419af1f55e62754bd36915ab6d181.

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // This code logs the user out by destoying the session and redirecting to login page.
-    session_start();
-    session_unset();
-    session_destroy();
-    session_write_close();
-    header('Location: index.php');
-    die();
-?>
